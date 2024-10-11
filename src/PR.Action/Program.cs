c9539ca7f9e65b3cac85a5c9435982fa7d65cc6d
Arda Cetinkaya <ardacetinkaya@gmail.com>
2024-10-11 20:26:14 +0200

    Update

--- src/PR.Action/Program.cs
+++ src/PR.Action/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure;
+using Azure.AI.Inference;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
+Settings settings = config.Get<Settings>() 
+    ?? throw new Exception("Invalid Configuration");
+
+IChatClient client = new ChatCompletionsClient(
+    endpoint: new Uri(settings.URI),
+    credential: new AzureKeyCredential(settings.APIKey)
+    ).AsChatClient(settings.ModelId);
+
+var messages = new List<ChatMessage>(){
+    new(Microsoft.Extensions.AI.ChatRole.System, $$"""
+    You are a software developer who knoe C# very well.
+    """)
+};
+
+messages.Add(new ChatMessage(){
+    Role= Microsoft.Extensions.AI.ChatRole.User,
+    Text = "Tell me about .NET 9"
+});
+ var result = await client.CompleteAsync(messages);
+
+ System.Console.WriteLine(result);
+
---
+++ src/PR.Action/Settings.cs
@@ -0,0 +1,6 @@
+public sealed class Settings
+{
+    public required string APIKey { get; set; }
+    public required string ModelId { get; set; }
+    public required string URI { get; set; }
+}
