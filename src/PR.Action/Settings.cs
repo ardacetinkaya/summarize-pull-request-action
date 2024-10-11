086d1e256eef78a299862e91bc0af223a6f3fe77
Arda Cetinkaya <ardacetinkaya@gmail.com>
2024-10-11 21:29:27 +0200

    Update

--- src/PR.Action/Settings.cs
+++ src/PR.Action/Settings.cs
@@ -3,6 +3,6 @@ public sealed class Settings
     public required string APIKey { get; set; }
     public required string ModelId { get; set; }
     public required string URI { get; set; }
-    public required string PullRequestId { get; set; }
+    public required string INPUT_PULLREQUESTID { get; set; }
     public required string PAT { get; set; }
 }
