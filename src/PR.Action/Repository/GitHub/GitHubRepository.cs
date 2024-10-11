f96bb667a48110aa3aa7afd4b7b319adc1fabe64
Arda Cetinkaya <ardacetinkaya@gmail.com>
2024-10-11 21:54:18 +0200

    Update

--- src/PR.Action/Repository/GitHub/GitHubRepository.cs
+++ src/PR.Action/Repository/GitHub/GitHubRepository.cs
@@ -10,12 +10,12 @@ public class GitHubRepository : IRepository
     {
         _client = new HttpClient();
         _client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.diff");
-        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Trim());
     }
 
     public async Task<string> GetPRDiff(string user, string repository, string pullRequestId)
     {
-        var response = await _client.GetAsync($"https://api.github.com/repos/{user}/{repository}/pulls/{pullRequestId}");
+        var response = await _client.GetAsync($"https://api.github.com/repos/ardacetinkaya/pull-request-action/pulls/1");
         response.EnsureSuccessStatusCode();
 
         var result = await response.Content.ReadAsStringAsync();
